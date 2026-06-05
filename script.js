270b0581469beefdaf0ea17363c3edd8c720be7b
varshasakthi800 <varshasakthi1972@gmail.com>
2026-06-05 14:28:40 +0530

    Change signIn function to a global function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let inputElement = document.getElementById("inputElement");
 let signInTextElement = document.getElementById("signInTextElement");
 
-function signIn() {
+window.signIn = function () {
     let inputValue = inputElement.value;
     let verifyText = "Hi " + inputValue + ", verifying your account...";
     signInTextElement.textContent = verifyText;
-}
+};
